5760f1cc8a37705611793880a6a65c378c339654
Ali SH Ihwedi <Ali SH Ihwedi@LAYNPC>
2017-08-30 22:41:14 +0300

    Add project files.

---
+++ testConsole1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] student = { "Ali", "Ahmed", "Jafer", "Joar" };
+            Console.WriteLine("Enter the student name for search");
+            string studentName = Console.ReadLine();
+            bool contains = false;
+            for (int i = 0; i < student.Length; i++)
+            {
+                if (studentName == student[i])
+                {
+                    contains = true;
+                }
+            }
+            if (contains == true)
+            {
+                Console.WriteLine("The student is in the class");
+            }
+            else
+            {
+                Console.WriteLine("The student is not in the class");
+            }
+
+            //int[] grades = { 55, 58, 66, 77, 88 };
+            //for (int i = 0; i<grades.Length;i++)
+            //{
+            //    grades[i] += 3;
+            //    Console.WriteLine(grades[i]);
+            //}
+        }
+    }
+}
